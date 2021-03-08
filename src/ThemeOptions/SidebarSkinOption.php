84fdfada3655d2585ef77a661a2e0a6485d9aa26
bishwopl <bishwo.prasad@gmail.com>
2021-03-08 21:18:12 +0545

    Sidebar skin option

---
+++ src/ThemeOptions/SidebarSkinOption.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LaminasAdminLTE\ThemeOptions;
+
+/**
+ * @author Bishwo Prasad Lamichhane <bishwo.prasad@gmail.com>
+ */
+class SidebarSkinOption {
+
+    public static $sidebarDarkPrimary    = 'sidebar-dark-primary';
+    public static $sidebarDarkWarning    = 'sidebar-dark-warning';
+    public static $sidebarDarkInfo       = 'sidebar-dark-info';
+    public static $sidebarDarkDanger     = 'sidebar-dark-danger';
+    public static $sidebarDarkSuccess    = 'sidebar-dark-success';
+    public static $sidebarDarkIndigo     = 'sidebar-dark-indigo';
+    public static $sidebarDarkLightblue  = 'sidebar-dark-lightblue';
+    public static $sidebarDarkNavy       = 'sidebar-dark-navy';
+    public static $sidebarDarkPurple     = 'sidebar-dark-purple';
+    public static $sidebarDarkFuchsia    = 'sidebar-dark-fuchsia';
+    public static $sidebarDarkPink       = 'sidebar-dark-pink';
+    public static $sidebarDarkMaroon     = 'sidebar-dark-maroon';
+    public static $sidebarDarkOrange     = 'sidebar-dark-orange';
+    public static $sidebarDarkLime       = 'sidebar-dark-lime';
+    public static $sidebarDarkTeal       = 'sidebar-dark-teal';
+    public static $sidebarDarkOlive      = 'sidebar-dark-olive';
+    public static $sidebarLightPrimary   = 'sidebar-light-primary';
+    public static $sidebarLightWarning   = 'sidebar-light-warning';
+    public static $sidebarLightInfo      = 'sidebar-light-info';
+    public static $sidebarLightDanger    = 'sidebar-light-danger';
+    public static $sidebarLightSuccess   = 'sidebar-light-success';
+    public static $sidebarLightIndigo    = 'sidebar-light-indigo';
+    public static $sidebarLightLightblue = 'sidebar-light-lightblue';
+    public static $sidebarLightNavy      = 'sidebar-light-navy';
+    public static $sidebarLightPurple    = 'sidebar-light-purple';
+    public static $sidebarLightFuchsia   = 'sidebar-light-fuchsia';
+    public static $sidebarLightPink      = 'sidebar-light-pink';
+    public static $sidebarLightMaroon    = 'sidebar-light-maroon';
+    public static $sidebarLightOrange    = 'sidebar-light-orange';
+    public static $sidebarLightLime      = 'sidebar-light-lime';
+    public static $sidebarLightTeal      = 'sidebar-light-teal';
+    public static $sidebarLightOlive     = 'sidebar-light-olive';
+
+}
