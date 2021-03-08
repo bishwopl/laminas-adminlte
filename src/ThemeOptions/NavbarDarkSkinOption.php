bc1ae3daeac9b78458eef88718182c2c6308b88b
bishwopl <bishwo.prasad@gmail.com>
2021-03-08 21:10:41 +0545

    Navbar dark skin options

---
+++ src/ThemeOptions/NavbarDarkSkinOption.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LaminasAdminLTE\ThemeOptions;
+
+/**
+ * @author Bishwo Prasad Lamichhane <bishwo.prasad@gmail.com>
+ */
+class NavbarDarkSkinOption {
+
+    public static $navbarPrimary   = 'navbar-dark navbar-primary';
+    public static $navbarSecondary = 'navbar-dark navbar-secondary';
+    public static $navbarInfo      = 'navbar-dark navbar-info';
+    public static $navbarSuccess   = 'navbar-dark navbar-success';
+    public static $navbarDanger    = 'navbar-dark navbar-danger';
+    public static $navbarIndigo    = 'navbar-dark navbar-indigo';
+    public static $navbarPurple    = 'navbar-dark navbar-purple';
+    public static $navbarPink      = 'navbar-dark navbar-pink';
+    public static $navbarNavy      = 'navbar-dark navbar-navy';
+    public static $navbarLightblue = 'navbar-dark navbar-lightblue';
+    public static $navbarTeal      = 'navbar-dark navbar-teal';
+    public static $navbarCyan      = 'navbar-dark navbar-cyan';
+    public static $navbarDark      = 'navbar-dark navbar-dark';
+    public static $navbarGrayDark  = 'navbar-dark navbar-gray-dark';
+    public static $navbarGray      = 'navbar-dark navbar-gray';
+
+}
