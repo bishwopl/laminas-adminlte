7d7d15fb764d49a2a19c1339f98ccebfc9dc9b02
bishwopl <bishwo.prasad@gmail.com>
2021-03-08 21:14:44 +0545

    Light Color option for navbar

---
+++ src/ThemeOptions/NavbarLightSkinOption.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace LaminasAdminLTE\ThemeOptions;
+
+/**
+ * @author Bishwo Prasad Lamichhane <bishwo.prasad@gmail.com>
+ */
+class NavbarLightSkinOption {
+
+    public static $navbarLight   = 'navbar-light navbar-light';
+    public static $navbarWarning = 'navbar-light navbar-warning';
+    public static $navbarWhite   = 'navbar-light navbar-white';
+    public static $navbarOrange  = 'navbar-light navbar-orange';
+
+}
