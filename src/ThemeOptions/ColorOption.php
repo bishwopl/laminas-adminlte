e74feef64e5a26cd47112b41e1571a3deb406961
bishwopl <bishwo.prasad@gmail.com>
2021-03-08 21:14:13 +0545

    Color option for navbar and brand logo

---
+++ src/ThemeOptions/ColorOption.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace LaminasAdminLTE\ThemeOptions;
+
+/**
+ * @author Bishwo Prasad Lamichhane <bishwo.prasad@gmail.com>
+ */
+class ColorOption {
+
+    public static $bgPrimary   = 'navbar-primary';
+    public static $bgWarning   = 'navbar-warning';
+    public static $bgInfo      = 'navbar-info';
+    public static $bgDanger    = 'navbar-danger';
+    public static $bgSuccess   = 'navbar-success';
+    public static $bgIndigo    = 'navbar-indigo';
+    public static $bgLightblue = 'navbar-lightblue';
+    public static $bgNavy      = 'navbar-navy';
+    public static $bgPurple    = 'navbar-purple';
+    public static $bgFuchsia   = 'navbar-fuchsia';
+    public static $bgPink      = 'navbar-pink';
+    public static $bgMaroon    = 'navbar-maroon';
+    public static $bgOrange    = 'navbar-orange';
+    public static $bgLime      = 'navbar-lime';
+    public static $bgTeal      = 'navbar-teal';
+    public static $bgOlive     = 'navbar-olive';
+    public static $bgLight     = 'navbar-light';
+    public static $bgWhite     = 'navbar-white';
+
+}
