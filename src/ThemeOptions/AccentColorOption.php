64fa9c691878dbe45e3d41a75873060dbf8e71bf
bishwopl <bishwo.prasad@gmail.com>
2021-03-08 21:18:25 +0545

    Accent color option

---
+++ src/ThemeOptions/AccentColorOption.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace LaminasAdminLTE\ThemeOptions;
+
+/**
+ * @author Bishwo Prasad Lamichhane <bishwo.prasad@gmail.com>
+ */
+class AccentColorOption {
+
+    public static $accentPrimary   = 'accent-primary';
+    public static $accentWarning   = 'accent-warning';
+    public static $accentInfo      = 'accent-info';
+    public static $accentDanger    = 'accent-danger';
+    public static $accentSuccess   = 'accent-success';
+    public static $accentIndigo    = 'accent-indigo';
+    public static $accentLightblue = 'accent-lightblue';
+    public static $accentNavy      = 'accent-navy';
+    public static $accentPurple    = 'accent-purple';
+    public static $accentFuchsia   = 'accent-fuchsia';
+    public static $accentPink      = 'accent-pink';
+    public static $accentMaroon    = 'accent-maroon';
+    public static $accentOrange    = 'accent-orange';
+    public static $accentLime      = 'accent-lime';
+    public static $accentTeal      = 'accent-teal';
+    public static $accentOlive     = 'accent-olive';
+
+}
